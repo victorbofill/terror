af4f784e34dcf0bf257bd144bf8507658dc68188
Victor Bofill <victorm.bofill@gmail.com>
2020-09-01 12:37:51 -0700

    updated env example

--- env.example.ts
+++ env.example.ts
@@ -1,27 +1,9 @@
 // this is an example of what the env.ts file looks like, though the actual values are not uploaded to github
-export const API_KEY = 'api key';
-
-export const MEMBER_IDS = {
-    SAM: 'id number',
-    KASEY: 'id number',
-    VICTOR: 'id number',
-    DEAN: 'id number',
-    AUSTIN: 'id number',
-    JACY: 'id number'
-};
-
-export const VOICE_CHANNEL_IDS = {
-    GENERAL: 'id number',
-    QUIET_PLACE: 'id number',
-    SNORING_CHANNEL: 'id number'
-}
-
-export const CHAT_CHANNEL_IDS = {
-    ROLE_PLAYING_GEEKERY: 'id number'
-}
+export const API_KEY = 'discord server API';
 
 export const SERVER_IDS = {
-    THE_BOYS: 'id number'
+    CLIENT_1: 'client id',
+    CLIENT_2: 'client id'
 }
 
-export const TERROR_ID = 'id number';
+export const BOT_ID = 'bot guild id';
